dd41d96d3b4e8d3136382eaebc0df49fba86fd7a
LSMA <stanasjes@gmail.com>
2016-08-01 18:11:12 -0500

    Changed static/media urls

--- kwky/settings.py
+++ kwky/settings.py
@@ -119,11 +119,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/assets/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
 
 # Upload stuff
 MEDIA_ROOT = os.path.join(BASE_DIR, 'uploads')
-MEDIA_URL = '/uploads/'
+MEDIA_URL = '/assets/uploads/'
