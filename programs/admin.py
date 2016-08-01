03e5ada8cf57545327a23aa2be9c0d8768677435
LSMA <stanasjes@gmail.com>
2016-08-01 18:03:31 -0500

    Added programs app models to admin site; changed StaffProfile.bio to TextField

--- programs/admin.py
+++ programs/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import StaffProfile, Program
+
+admin.site.register(StaffProfile)
+admin.site.register(Program)
+
--- programs/models.py
+++ programs/models.py
@@ -1,29 +1,5 @@
 from django.db import models
 
-class StaffProfile(models.Model):
-    first_name = models.CharField(max_length=32)
-    last_name = models.CharField(max_length=32)
-    job_title = models.CharField(max_length=64)
-    picture = models.ImageField(upload_to='staff/')
-    bio = models.CharField(max_length=1048)
-    program = models.ForeignKey(
-        Program,
-        null=True,
-        blank=True,
-        on_delete=models.SET_NULL,
-    )
-
-
-class Link(models.Model):
-    display_text = models.CharField(max_length=128)
-    href = models.URLField('Link URL')
-    staff = models.ForeignKey(Host, on_delete=models.CASCADE)
-
-class Showtime(models.Model):
-    start_time = TimeField()
-    duration = DurationField()
-    program = models.ForeignKey(Program, on_delete=models.CASCADE)
-    
 
 class Program(models.Model):
     DAY_CHOICES = (('S','Sun'),
@@ -38,6 +14,34 @@ class Program(models.Model):
                    ('X','Every Day'),
                    (None, 'Day of Week'))
     title = models.CharField(max_length=128)
-    abbr = SlugField('Program ID', max_length=3)
-    air_days = TextField(choices=DAY_CHOICES)
+    abbr = models.SlugField('Program ID', max_length=3)
+    air_days = models.TextField(choices=DAY_CHOICES)
     
+
+class StaffProfile(models.Model):
+    first_name = models.CharField(max_length=32)
+    last_name = models.CharField(max_length=32)
+    job_title = models.CharField(max_length=64)
+    picture = models.ImageField(upload_to='staff/')
+    email = models.EmailField('Contact Email')
+    phone = models.CharField('Phone Number',max_length=14)
+    bio = models.TextField()
+    program = models.ForeignKey(
+        Program,
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+    )
+
+
+class Link(models.Model):
+    display_text = models.CharField(max_length=128)
+    href = models.URLField('Link URL')
+    staff = models.ForeignKey(StaffProfile, on_delete=models.CASCADE)
+
+class Showtime(models.Model):
+    start_time = models.TimeField()
+    duration = models.DurationField()
+    program = models.ForeignKey(Program, on_delete=models.CASCADE)
+    
+
