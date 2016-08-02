7450607fb8f151a9314b5c21227067165e7d810d
LSMA <stanasjes@gmail.com>
2016-08-02 01:14:12 -0500

    Fixed syntax error in kwky/urls.py

--- kwky/urls.py
+++ kwky/urls.py
@@ -15,14 +15,14 @@ Including another URLconf
 """
 from django.conf.urls import url, include
 from django.contrib import admin
-from django.views import static.serve
+from django.views.static import serve
 
 import programs.views 
 
 urlpatterns = [
     url(r'^programs/', include('programs.program_urls')),
     url(r'^staff/', include('programs.staff_urls')),
-    url(r'^assets/uploads/(?P<path>.*)$', static.serve),
-    url(r'^assets/static/(?P<path>.*)$', static.serve),
+    url(r'^assets/uploads/(?P<path>.*)$', serve),
+    url(r'^assets/static/(?P<path>.*)$', serve),
     url(r'^admin/', admin.site.urls),
 ]
