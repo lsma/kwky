82f57275542e4e5068ed5dd62df78550c73511b5
LSMA <stanasjes@gmail.com>
2016-08-01 19:03:44 -0500

    Fixed bug in programs/staff_urls.py: had \S in staff_detail pattern (should have been \w)

--- programs/staff_urls.py
+++ programs/staff_urls.py
@@ -4,5 +4,5 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.staff_index, name='staff_index'),
-    url(r'^(?P<name>\S+)/$', views.staff_detail, name='staff_detail'),
+    url(r'^(?P<name>\w+)/$', views.staff_detail, name='staff_detail'),
 ]
