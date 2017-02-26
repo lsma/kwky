e2bc3532edc5f6182cbe1553af9fb71f74e69854
Stan <stanasjes@gmail.com>
2017-02-26 03:48:00 +0000

    fixed up program index/pages.  styling wip

--- programs/templatetags/programs.py
+++ programs/templatetags/programs.py
@@ -3,7 +3,7 @@ from programs.models import Program
 
 register = template.Library()
 
-@register.inclusion_tag('template_menu.html')
+@register.inclusion_tag('program_submenu.html')
 def program_menu():
     programs = Program.objects.order_by('title')
     return {'programs': programs}
--- programs/views.py
+++ programs/views.py
@@ -6,7 +6,7 @@ from django.http import Http404,HttpResponseServerError
 from django.shortcuts import render, get_object_or_404
 from django.conf import settings
 
-from .models import StaffProfile, Program
+from .models import StaffProfile, Program, Link
 
 # Soundcloud client option for retrieving soundcloud track IDs for embeded
 # player in /programs/[abbr]/[mmddyy]
@@ -37,6 +37,7 @@ def program_detail(request, prog_id):
     # Get the host's entry from the database
     hosts = StaffProfile.objects.filter(program=prog).order_by('org_rank')
 
+    links = Link.objects.filter(staff__in=hosts)
 
     ## Get all soundcloud playlists for this program
     ## (there is a playlist for each year)
@@ -86,6 +87,7 @@ def program_detail(request, prog_id):
     context = {'prog': prog,
                'sc_embed_src': 'http://www.reddit.com',
                'hosts': hosts,
+               'links': links,
                'playlists': prog_playlists,
                'newest_track': newest_track,}
     return render(request, 'programs/program_detail.html', context)
