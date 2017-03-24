8535972b39a49772b4a2c5c4be95bd885c021d56
Stan <stanasjes@gmail.com>
2017-03-24 13:46:38 +0000

    added markdown tag for events

--- events/templatetags/events.py
+++ events/templatetags/events.py
@@ -1,9 +1,32 @@
+import datetime, calendar
+
+from markdown import markdown as md
+
 from django import template
+from django.utils.html import conditional_escape
+from django.utils.safestring import mark_safe
+from django.template.defaultfilters import stringfilter
+
 from events.models import Event
+
+MIN_TIME = datetime.datetime(datetime.MINYEAR, 1, 1)
+MAX_TIME = datetime.datetime(datetime.MAXYEAR, 12, calendar.monthrange(datetime.MAXYEAR,12)[1])
 
 register = template.Library()
 
+@register.filter(needs_autoescape=True)
+@stringfilter
+def markdown(value, autoescape=True):
+    """Converts markdown text into html"""
+    #if autoescape:
+    #    esc = conditional_escape
+    #else:
+    #    esc = lambda x: x
+    return mark_safe(md(conditional_escape(value)))
+
 @register.inclusion_tag('event_submenu.html')
 def event_menu():
-    events = Event.objects.order_by('weight')
+    events = Event.objects.filter(expire__range=(datetime.datetime.now(), MAX_TIME),
+                                  begin__range=(MIN_TIME, datetime.datetime.now()),
+                           ).order_by('weight')
     return {'events': events}
