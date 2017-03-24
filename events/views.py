9911bf262cdb7c6fba5d0ae3c2c6687b6b3d08af
Stan <stanasjes@gmail.com>
2017-03-24 13:54:03 +0000

    event index only shows events that haven't expired

--- events/views.py
+++ events/views.py
@@ -1,9 +1,16 @@
+import datetime, calendar
+
 from django.shortcuts import render, get_object_or_404
 
 from .models import Event
 
+MIN_TIME = datetime.datetime(datetime.MINYEAR, 1, 1)
+MAX_TIME = datetime.datetime(datetime.MAXYEAR, 12, calendar.monthrange(datetime.MAXYEAR,12)[1])
+
 def event_index(request):
-    events = Event.objects.order_by('title')
+    events = Event.objects.filter(expire__range=(datetime.datetime.now(), MAX_TIME),
+                                  begin__range=(MIN_TIME, datetime.datetime.now()),
+                           ).order_by('weight')
     context = {'event_list': events}
     return render(request, 'events/event_index.html', context)
 
