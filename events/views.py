aea5142aecc08027e16529b08992a282a3ffed43
Stan Asjes <stan@kwky.com>
2017-03-28 22:08:35 +0000

    Switched from datetime.datetime.now() to Django

--- events/views.py
+++ events/views.py
@@ -1,5 +1,6 @@
 import datetime, calendar
 
+from django.utils import timezone
 from django.shortcuts import render, get_object_or_404
 
 from .models import Event
@@ -8,8 +9,9 @@ MIN_TIME = datetime.datetime(datetime.MINYEAR, 1, 1)
 MAX_TIME = datetime.datetime(datetime.MAXYEAR, 12, calendar.monthrange(datetime.MAXYEAR,12)[1])
 
 def event_index(request):
-    events = Event.objects.filter(expire__range=(datetime.datetime.now(), MAX_TIME),
-                                  begin__range=(MIN_TIME, datetime.datetime.now()),
+    now = timezone.now()
+    events = Event.objects.filter(expire__range=(now, MAX_TIME),
+                                  begin__range=(MIN_TIME, now),
                            ).order_by('weight')
     context = {'event_list': events}
     return render(request, 'events/event_index.html', context)
